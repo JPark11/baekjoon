db0c5da372c8eb5b0f2a9a69bf9e188acb34f1d3
JPark11 <62351890+JPark11@users.noreply.github.com>
2023-04-04 22:16:42 +0900

    [Gold III] Title: 소수의 연속합, Time: 2228 ms, Memory: 77992 KB -BaekjoonHub

---
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.py
@@ -0,0 +1,34 @@
+N = int(input())
+
+import math 
+
+num_list = [True for i in range(N+1)]
+num_list[0] = False 
+num_list[1] = False
+
+for i in range(2, int(math.sqrt(N)) + 1):
+    if num_list[i]:
+        j = 2 
+        while i * j <= N: 
+            num_list[i * j] = False 
+            j += 1 
+
+prime = [idx for idx, v in enumerate(num_list) if v]
+
+
+length = len(prime)
+end = 0 
+count = 0
+prime_sum = 0 
+
+for start in range(length):
+    while prime_sum < N and end < length:
+        prime_sum += prime[end]
+        end += 1 
+    if prime_sum == N: 
+        count += 1 
+    prime_sum -= prime[start]
+
+
+print(count)
+
