f5ab67a0f0c02b97a232700a5c5758baf1560572
JPark11 <62351890+JPark11@users.noreply.github.com>
2023-06-06 22:44:27 +0900

    [Silver IV] Title: 큐, Time: 72 ms, Memory: 34120 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.py
@@ -0,0 +1,32 @@
+from collections import deque 
+import sys 
+
+input = sys.stdin.readline
+
+N = int(input().rstrip())
+q = deque() 
+for _ in range(N):
+    raw = input().rstrip()
+
+    if raw[:4] == 'push':
+        q.append(raw.split()[1])
+    elif raw == 'pop':
+        if q: 
+            print(q.popleft())
+        else:
+            print(-1)
+    elif raw == 'size':
+        print(len(q))
+    elif raw == 'empty':
+        res = 0 if q else 1 
+        print(res)
+    elif raw == 'front':
+        if q: 
+            print(q[0])
+        else:
+            print(-1)
+    elif raw == 'back':
+        if q:
+            print(q[-1])
+        else:
+            print(-1)
